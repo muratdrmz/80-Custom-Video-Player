6ae18f62e4f6c105d52226a3ce41143b1effdbca
muratdrmz <mrtdrmz@windowslive.com>
2023-04-08 17:12:35 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+/* Build out functions */
+ 
+function togglePlay(){
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+  // video.paused?video.play():video.pause()
+}
+
+function updateButton(){ 
+ console.log('update the button');
+ console.log(this);
+ const icon = this.paused? "►" : "❚ ❚";
+ console.log(icon);
+ toggle.textContent=icon
+}
+
+function skip(){
+ console.log(this.dataset.skip);
+ video.currentTime+=parseFloat(this.dataset.skip)
+}
+
+function handleRangeUpdate(){
+ video[this.name]=this.value;
+
+}
+
+function handleProgress(){
+ const percent=(video.currentTime/video.duration)*100;
+ console.log(percent);
+ progressBar.style.flexBasis=`${percent}%`;
+}
+
+function scrub(e){
+ console.log(e.offsetX,progress.offsetWidth);
+ const scrubTime=(e.offsetX/progress.offsetWidth)*video.duration;
+ video.currentTime=scrubTime
+}
+
+// event listeners
+
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
+
+toggle.addEventListener("click", togglePlay);
+skipButtons.forEach(button=>{button.addEventListener('click',skip)})
+// ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) => range.addEventListener("mousemove", handleRangeUpdate));
+
+let mousedown=false;
+
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e)=>mousedown&&scrub(e));
+progress.addEventListener("mousedown", ()=>mousedown=true);
+progress.addEventListener("mouseup", ()=>mousedown=false);
+ 
+
+
+
+window.addEventListener('keypress',(e)=>{
+if (e.key === " " && video.paused){
+video.play()
+}else if(e.key === " " && video.played){
+ video.pause()
+}
+})
+
+document.addEventListener("keydown", (e) => {
+ console.log(e.key);
+if (e.key === "Control" && video.requestFullscreen) video.requestFullscreen();
+}
+);
